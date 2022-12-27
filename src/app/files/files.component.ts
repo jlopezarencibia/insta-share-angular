f9458b45ef5a413cc90535737f0362c834ed391a
Jose Lopez <jose.lopez@isucorp.ca>
2022-12-26 23:31:32 -0500

    Basic layout and behavioour

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, Injector} from '@angular/core';
+import {FileParameter, UserFileServiceProxy} from '@shared/service-proxies/service-proxies';
+import {AppComponentBase} from '@shared/app-component-base';
+import {firstValueFrom} from '@node_modules/rxjs';
 
 @Component({
-  selector: 'app-files',
-  templateUrl: './files.component.html',
-  styleUrls: ['./files.component.css']
+    selector: 'app-files',
+    templateUrl: './files.component.html',
+    styleUrls: ['./files.component.css']
 })
-export class FilesComponent {
+export class FilesComponent extends AppComponentBase {
 
+    constructor(
+        injector: Injector,
+        private readonly fileService: UserFileServiceProxy) {
+        super(injector);
+    }
+
+    upload(inputFile: HTMLInputElement) {
+
+    }
+
+    async fileChanged(files: FileList) {
+        for (let i = 0; i < files.length; i++) {
+            const fileToUpload = files.item(i);
+            const formFile: FileParameter = {
+                fileName: fileToUpload.name,
+                data: fileToUpload
+            };
+            const result = await firstValueFrom(this.fileService.uploadFile(this.appSession.userId, formFile));
+            console.log(result);
+            console.log('uploaded');
+            // this.http.post('https://localhost:44311/api/services/app/UserFile/UploadFile',
+            //     formData).subscribe((response) => console.log(response), error => console.log(error));
+        }
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,24 +18,4 @@ export class HomeComponent extends AppComponentBase {
         super(injector);
     }
 
-    async fileChanged(files: FileList) {
-        for (let i = 0; i < files.length; i++) {
-            const fileToUpload = files.item(i);
-            // const formData = new FormData();
-            // formData.append('UserId', `${this.appSession.userId.toString()}`);
-            // formData.append('File', fileToUpload);
-            // const formData = [new StringStringValuesKeyValuePair({
-            //     key: 'UserId', value: [`${this.appSession.userId.toString()}`]
-            // })];
-            const formFile: FileParameter = {
-                fileName: fileToUpload.name,
-                data: fileToUpload
-            };
-            const result = await firstValueFrom(this.userFileService.uploadFile(this.appSession.userId, formFile));
-            console.log(result);
-            console.log('uploaded');
-            // this.http.post('https://localhost:44311/api/services/app/UserFile/UploadFile',
-            //     formData).subscribe((response) => console.log(response), error => console.log(error));
-        }
-    }
 }
