2f797f1be601b3a6eff1a5e477884bfd528fd414
Jose Lopez <jose.lopez@isucorp.ca>
2022-12-27 13:04:22 -0500

    Added download action

--- src/shared/components/file-information/file-information.component.ts
+++ src/shared/components/file-information/file-information.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {BasicUaserFileDto, UserFileServiceProxy} from '@shared/service-proxies/service-proxies';
 import {firstValueFrom} from '@node_modules/rxjs';
+import {browser} from '@node_modules/protractor';
 
 @Component({
     selector: 'app-file-information',
@@ -45,12 +46,24 @@ export class FileInformationComponent {
         this.loading = false;
     }
 
-    download() {
-
+    async download() {
+        this.loading = true;
+        const file = await firstValueFrom(this.fileService.get(this.file.id));
+        console.log(file);
+        const blob = new Blob([file.fileBytes]);
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = file.fileName;
+        link.click();
+        this.action.emit(FileAction.Download);
+        this.loading = false;
     }
 
-    remove() {
-
+    async remove() {
+        this.loading = true;
+        await firstValueFrom(this.fileService.delete(this.file.id));
+        this.action.emit(FileAction.Remove);
+        this.loading = false;
     }
 }
 
